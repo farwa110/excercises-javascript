45a993cad806602344648366d4701db6dfb937b6
farwa110 <farwamehdi3@gmail.com>
2025-01-28 12:50:46 +0100

    yes

---
+++ ovelser1a/script.js
@@ -0,0 +1,13 @@
+// sayHello("Farwa");
+// function sayHello(firstname) {
+//   console.log(`Hello ${firstname}`);
+// }
+
+const myName = "Mona"; // Variable myName, can use let or const eller myName= "Mona";
+
+function greetName(name) {
+  // Function greetName instead of myName
+  console.log(`Name: ${name}`);
+}
+
+greetName(myName); // Pass the variable myName to the function
