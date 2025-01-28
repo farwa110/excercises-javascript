353047ae6c69f026133dbc982ac54a0faa52402c
farwa110 <farwamehdi3@gmail.com>
2025-01-28 13:04:04 +0100

    done

---
+++ ovelser2a/index.js
@@ -0,0 +1,4 @@
+presentPet("Klaus", "dog", "Mona");
+function presentPet(firstName, animalType, petName) {
+  console.log(`My name is ${firstName},i have a ${animalType},whose name is ${petName}`);
+}
