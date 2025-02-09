f5ecea39d8d2b274227e92142dd03eac1dd003df
Farwa Rizvi <farwamehdi3@gmail.com>
2025-02-09 13:47:20 +0100

    prep-two completed

---
+++ profanitypreptwo/prep.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let sentence = "Jeg er en tekst der skal findes ord i";
+
+const word = "ord";
+
+console.log(`The word "${word}" ${sentence.includes(word) ? "is" : "is not"} in the sentence`);
+
+//some()only works on array not on strings//
+//wordsToCheckFor.some(...) → Loops through each word in the array.
+//  word => theText.includes(word) → Checks if theText contains that word.
+//  some() → Returns true if at least one word is found in theText, otherwise false.
+
+const wordsToCheckFor = ["ord", "tekst"];
+console.log(wordsToCheckFor.some((word) => sentence.includes(word)));
